da333614f480119cf2d42d1401d6c11c92ca31db
user <user@antsdr-dev.localdomain>
2022-07-19 15:29:34 +0200

    Add preparation script for config parameters

---
+++ prepare.sh
@@ -0,0 +1,5 @@
+export CROSS_COMPILE=arm-linux-gnueabihf-
+export PATH=$PATH:/opt/Xilinx/SDK/2019.1/gnu/aarch32/lin/gcc-arm-linux-gnueabi/bin
+export VIVADO_SETTINGS=/opt/Xilinx/Vivado/2019.1/settings64.sh
+export PERL_MM_OPT=
+
